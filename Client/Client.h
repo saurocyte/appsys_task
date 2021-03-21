d1434ccef2fffd92075378e333d5c7d0bc037946
me_win <idk>
2021-03-22 01:22:03 +0300

    refactor: client stuff

--- Client/Client.h
+++ Client/Client.h
@@ -1,6 +1,32 @@
 #pragma once
+
+#include <WinSock2.h>
+#include <string>
+
+#include <SocketWrapper/Socket.h>
+
+typedef char Int8;
+typedef short int Int16;
+typedef int Int32;
+
+typedef std::vector<Command> CommandList;
+
+using namespace tinyxml2;
+
+
 class Client {
 public:
-	void run();
+    Client(PCSTR addr, PCSTR port, const std::string _commands_path);
+    void run();
+private:
+    void parse_commands();
+
+    std::string commands_path;
+    CommandList commands;
+
+    ClientSocket conn;
+    char recvbuf[512];
+    int recvbuflen = 512;
 };
+
 
